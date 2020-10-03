1a762bc478846cf63964728b52d858d88983c21e
Yi Nian <40296764+langzippkk@users.noreply.github.com>
2020-10-02 22:56:22 -0600

    Add files via upload

--- kmean/k_mean.py
+++ kmean/k_mean.py
@@ -1,8 +1,9 @@
 import numpy as np
 import os
 import pandas as pd
+from collections import defaultdict
 
-## mainly use pandas
+
 def compute_distance(point,centroid):
 	return np.sqrt(np.sum(point-centroid)**2)
 
@@ -15,11 +16,9 @@ def initialize_centroids(data_points,k):
 def iteration(data_point,num_of_cluster,number_of_iteration):
 	centroid = initialize_centroids(data_point,num_of_cluster)
 	k = len(centroid)
-	cluster = {}
-	for i in range(k):
-		cluster[i] = []
 	## use defaultdict
 	for iter in range((number_of_iteration)):
+		cluster = defaultdict(list)
 		for i in range(len(data_point)):
 			dist = []
 			## E-step, assign data to clusters according to their closest centroid
@@ -27,20 +26,17 @@ def iteration(data_point,num_of_cluster,number_of_iteration):
 			for j in range(k):
 				dist.append(compute_distance(data,centroid[j]))
 			index_temp = np.argmin(dist)
-			if iter == number_of_iteration-1:
-				cluster[index_temp].append(data)
+			cluster[index_temp].append(data)
 		## M step 
 		## calculate new centroid using means
 		for i in range(k):
 			centroid[i] = np.mean(cluster[i])
+		print(centroid)
 	return cluster
 
 if __name__ == "__main__":
     data_points = pd.read_csv('Kmean.csv')
-    columns = data_points.columns
-    for i in columns:
-        data_points[i]= data_points[i].astype(float)
-    total_iteration = 1
-    number_of_cluster = 3
+    total_iteration = 5
+    number_of_cluster = 2
     res = iteration(data_points,number_of_cluster,total_iteration)
     print(res)
